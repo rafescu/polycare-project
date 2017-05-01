cc33f5130aac8de9e97f3c18ce4f917220322f69
rafescu <rafael-tome@hotmail.com>
2017-05-01 02:16:51 +0100

    Create class MedicosDB and Pacientes

---
+++ PolyCare/PolyCare/Models/Pacientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PolyCare.Models {
+    public class Pacientes {
+        public int ID { get; set; }
+
+        public int CodPaciente { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
