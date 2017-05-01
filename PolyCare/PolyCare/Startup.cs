1533ebc9bd5980d81b8c24cb737ab49751fb6b83
rafescu <rafael-tome@hotmail.com>
2017-05-01 01:32:05 +0100

    Initial Commit

---
+++ PolyCare/PolyCare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PolyCare.Startup))]
+namespace PolyCare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
